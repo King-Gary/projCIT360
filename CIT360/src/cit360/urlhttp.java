8db46d217d956cbb1c45f0b5debcb77a096a5105
Gary <Gary@DESKTOP-N5T8JS1>
2019-02-11 22:33:46 -0700

    added url-http request.

---
+++ CIT360/src/cit360/urlhttp.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit360;
+
+/**
+ *
+ * @author Gary
+ */
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+public class urlhttp {
+    public static void main(String[] args) throws Exception {
+         Scanner keyboard = new Scanner(System.in);
+        System.out.println("What url would you like to test?");
+        String webaddress = keyboard.nextLine();
+        URL url = new URL(webaddress);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+
+    private static void readStream(InputStream in) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+  
+}
