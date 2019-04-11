dcdbcdc0db5111e1efe99f5b0ce4b5bd4954b9f7
Gary <Gary@DESKTOP-N5T8JS1>
2019-04-10 23:08:27 -0400

    added all needed classes

---
+++ CIT360/Test/Multiply.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import static org.junit.Assert.*;
+
+
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Gary
+ */
+public class Multiply {
+    	
+
+	public void testMultiply() {
+
+		
+            @test
+		Junit test = new  Junit();
+
+		int result = test.multiply(3, 4);
+
+		assertEquals(12, result);
+
+		
+}
+
+
+}
